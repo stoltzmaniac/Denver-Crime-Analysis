ca37fc20062409c969b485231fc1350d807167de
Unknown <scott@stoltzmanconsulting.com>
2017-01-21 16:41:39 -0700

    Added Shiny App

---
+++ shinyApp/server.R
@@ -0,0 +1,45 @@
+library(RSQLite)
+library(dplyr)
+library(lubridate)
+library(ggmap)
+
+con = dbConnect(RSQLite::SQLite(),dbname='crime.sqlite')
+rs = dbSendQuery(con, "SELECT * FROM crime") 
+data = dbFetch(rs)
+data$date = as.Date(data$REPORTED_DATE)
+data$year = year(data$date)
+data$month = month(data$date)
+data$day = day(data$date)
+data$hour = hour(data$REPORTED_DATE)
+
+df = data %>%
+  select(date,year,month,day,hour,
+         OFFENSE_TYPE_ID,OFFENSE_CATEGORY_ID,
+         GEO_LAT,GEO_LON,NEIGHBORHOOD_ID)
+
+rm(data)
+
+pMap = ggmap(get_map(location = "Denver, Colorado, United States",
+                     zoom=12,
+                     maptype = 'terrain',
+                     color = "bw")) 
+
+shinyServer(
+  function(input, output){
+    
+    output$categoryList = unique(df$OFFENSE_CATEGORY_ID)
+    output$neighborhoodList = unique(df$NEIGHBORHOOD_ID)
+    output$typeList = unique(df$OFFENSE_TYPE_ID)
+    
+    output$chart = renderPlot({
+      pMap + geom_point(data=df %>%
+                          filter(date == '2016-01-01'),
+                        aes(x=GEO_LON,
+                            y=GEO_LAT,
+                            col=NEIGHBORHOOD_ID)
+                        ) + 
+        theme(legend.position='none')
+      
+    })
+  }
+)
---
+++ shinyApp/ui.R
@@ -0,0 +1,21 @@
+shinyUI(fluidPage(
+  titlePanel("Denver Crime Data"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      helpText("Create demographic maps with 
+        information from the 2010 US Census."),
+      
+      selectInput("var", 
+                  label = "Choose a variable to display",
+                  choices = outp,
+                  selected = ),
+      
+      sliderInput("range", 
+                  label = "Range of interest:",
+                  min = 0, max = 100, value = c(0, 100))
+    ),
+    
+    mainPanel(plotOutput("chart"))
+  )
+))
