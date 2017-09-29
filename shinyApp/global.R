34e70a123727227f80f49950c6560a60d6112a6d
Scott Stoltzman <scott@stoltzmanconsulting.com>
2017-09-29 16:51:54 -0600

    added date formatting

--- shinyApp/global.R
+++ shinyApp/global.R
@@ -8,9 +8,9 @@ library(plotly)
 crime = read_csv('../data/crime.csv')
 
 df = crime %>%
-  mutate(Date = as.Date(REPORTED_DATE), reportedIncidents = 1) %>%
-  mutate(Year = year(Date), Month = month(Date), DayOfWeek = wday(Date, label=TRUE)) %>%
-  select(Date, Month, DayOfWeek, FIRST_OCCURRENCE_DATE, LAST_OCCURRENCE_DATE, REPORTED_DATE,
+  mutate(Date = mdy_hms(REPORTED_DATE), reportedIncidents = 1) %>%
+  mutate(Year = year(Date), Month = month(Date), DayOfWeek = wday(Date, label=TRUE), Hour = hour(Date)) %>%
+  select(Date, Month, DayOfWeek, Hour, FIRST_OCCURRENCE_DATE, LAST_OCCURRENCE_DATE, REPORTED_DATE,
          OFFENSE_TYPE_ID,OFFENSE_CATEGORY_ID,
          GEO_LAT,GEO_LON,NEIGHBORHOOD_ID, DISTRICT_ID, PRECINCT_ID,
          IS_CRIME, IS_TRAFFIC,
